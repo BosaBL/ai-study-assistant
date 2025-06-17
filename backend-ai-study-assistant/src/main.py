e93a8fc169ca06d9cced5c01e60ae2d41e7afb6c
Christopher S. <43054246+BosaBL@users.noreply.github.com>
2025-06-16 23:30:06 -0400

    fix: cors issues

--- backend-ai-study-assistant/src/main.py
+++ backend-ai-study-assistant/src/main.py
@@ -26,11 +26,16 @@ load_dotenv()
 
 app = FastAPI(title="PDF Educational Content Generator", version="1.0.0")
 
-
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
